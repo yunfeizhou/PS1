c8468b7a020cf60441d551b06baece35ff7e853d
zhouyunf <zhou@Zhouyunfei.fios-router.home>
2015-03-27 02:06:55 +0800

    commit

---
+++ src/MainPackage/Main.java
@@ -0,0 +1,9 @@
+package MainPackage;
+
+public class Main {
+	public static void main(String[] args){
+	
+		// my account name is: Yunfeizhou
+	}
+
+}
---
+++ src/MainPackage/Yunfei.java
@@ -0,0 +1,13 @@
+package MainPackage;
+
+public class Yunfei {
+	public static void HelloWorld() {
+
+		System.out.println("Hello World");
+		
+		// My Github account name is: Yunfeizhou
+		
+		Yunfei.HelloWorld();
+	}
+
+}
